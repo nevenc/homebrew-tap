46b2cd4c708d265b3f437400454068f191102088
Neven C <nevenc@pivotal.io>
2019-08-06 17:44:57 +0100

    Bumped version to v3.0.0

--- om-cli.rb
+++ om-cli.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class OmCli < Formula
   homepage 'https://github.com/pivotal-cf/om'
-  version "2.0.1"
+  version "3.0.0"
   url "https://github.com/pivotal-cf/om/releases/download/#{version}/om-darwin-#{version}"
-  sha256 "63bda93dce923cced123ced4cbfea06d30b042b3dd9b9f9dc2eea99570b40ca2"
+  sha256 "a6a021f15c1290b019f24236de40a619ff5eb61de3cc55fc21602adf397c0a99"
 
   depends_on :arch => :x86_64
 
