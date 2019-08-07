e3bccb3c69b66d9c6f0066cb74c1c2bbf1a2384a
Neven C <nevenc@pivotal.io>
2019-08-07 14:39:24 +0100

    Removed download strategy

--- pace-cli.rb
+++ pace-cli.rb
@@ -3,7 +3,7 @@ require 'formula'
 class PaceCli < Formula
   homepage 'https://github.com/Pivotal-Field-Engineering/pace-builder'
   version "1.5"
-  url "https://github.com/Pivotal-Field-Engineering/pace-builder/releases/download/v#{version}/pace-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/Pivotal-Field-Engineering/pace-builder/releases/download/v#{version}/pace-darwin"
   sha256 "906b79cb00fe1773f1f633695b760a64a88f781077d9ea918385bb6553d5ab3b"
 
   depends_on :arch => :x86_64
